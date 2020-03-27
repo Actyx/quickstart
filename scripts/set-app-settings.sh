65923df134511a0814fa040e52f4222a49cb196b
Oliver Stollmann <oliver@stollmann.net>
2020-03-27 10:56:58 +0100

    packaging

---
+++ scripts/set-app-settings.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+command -v ax >/dev/null 2>&1 || { echo >&2 "Error: did not find 'ax'. Did you install the Actyx CLI? Aborting."; exit 1; }
+
+type="unkown"
+
+if [ "$1" = "webview" ]; then
+    type="webview"
+fi
+
+if [ "$1" = "docker" ]; then
+    type="docker"
+fi
+
+if [ $type = "unkown" ]; then
+    echo >&2 "Error: no or wrong app type '$1' given. Aborting."
+    echo >&2 "Usage: ./package-app.sh <webview | docker>"
+    exit 1;
+fi
+
+if [ $type = "docker" ]; then
+
+    if [[ "$(docker images -q sample-docker-app 2> /dev/null)" == "" ]]; then
+        echo >&2 "Error: cannot find 'sample-docker-app' image. Did you run 'npm run build:image' in 'sample-docker-app/'? Aborting."
+        exit 1;
+    fi
+fi
+
+app_id="com.actyx.sample-$type-app"
+echo "Setting empty settings for app '$app_id'"
+ax settings set --local $app_id "{}" localhost
